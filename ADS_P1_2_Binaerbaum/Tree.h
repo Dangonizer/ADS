37002b9d21a9ac41c3ce1aa1f29bfc5496336ac3
Dangonizer <schierkevin@gmail.com>
2024-05-01 16:43:37 +0200

    bst headers

--- ADS_P1_2_Binaerbaum/Tree.h
+++ ADS_P1_2_Binaerbaum/Tree.h
@@ -16,14 +16,24 @@ class Tree
 private:
     ///////////////////////////////////////
     // Ihr Code hier:
-
+    TreeNode* m_anker;
+    int m_currentNodeChronologicalID;
     //
     ////////////////////////////////////
 
 public:
     ///////////////////////////////////////
     // Ihr Code hier:
-    
+    Tree();
+    ~Tree();
+    void addNode(string name, int age, double income, int postcode);
+    bool deleteNode(int id);
+    bool searchNode(int name);
+    void printAll();
+    void printLevelOrder();
+    void printPreOrder();
+    void printInOrder();
+    void printPostOrder();
     //
     ////////////////////////////////////
     // friend-Funktionen sind für die Tests erforderlich und müssen unangetastet
--- ADS_P1_2_Binaerbaum/TreeNode.h
+++ ADS_P1_2_Binaerbaum/TreeNode.h
@@ -14,7 +14,14 @@ class TreeNode
 private:
     ///////////////////////////////////////
     // Ihr Code hier:
-
+    int m_NodeOrderId;
+    int m_NodeChronologicalID;
+    string m_Name;
+    int m_Age;
+    double m_Income;
+    int m_PostCode; 
+    TreeNode* m_left;
+    TreeNode* m_right;
     //
     ////////////////////////////////////
 
@@ -22,7 +29,23 @@ public:
     ///////////////////////////////////////
     // Ihr Code hier:
     TreeNode(int, int, string, int, double, int);
-
+    int get_m_NodeOrderId();
+    int get_m_NodeChronologicalID();
+    string get_m_Name();
+    int get_m_Age();
+    double get_m_Income();
+    int get_m_PostCode();
+    TreeNode* get_m_left();
+    TreeNode* get_m_right();
+    void set_m_NodeOrderId(int);
+    void set_m_NodeChronologicalID(int);
+    void set_m_Name(string);
+    void set_m_Age(int);
+    void set_m_Income(double);
+    void set_m_PostCode(int);
+    void set_m_left(TreeNode*);
+    void set_m_right(TreeNode*);
+    void print();
     //
     ////////////////////////////////////
 };
